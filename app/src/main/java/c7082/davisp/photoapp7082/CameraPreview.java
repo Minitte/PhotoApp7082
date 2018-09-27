9c34f396b46584eebcdf2abd36e01941b5fcc245
Davis Pham <arcentalminitte@gmail.com>
2018-09-26 17:53:55 -0700

    camera activity layout adjustments

--- app/src/main/java/c7082/davisp/photoapp7082/CameraPreview.java
+++ app/src/main/java/c7082/davisp/photoapp7082/CameraPreview.java
@@ -2,6 +2,7 @@ package c7082.davisp.photoapp7082;
 
 import android.content.Context;
 import android.hardware.Camera;
+import android.os.Build;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -25,7 +26,11 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         mHolder = getHolder();
         mHolder.addCallback(this);
         // deprecated setting, but required on Android versions prior to 3.0
-        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+//        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+            getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
     }
 
     public void surfaceCreated(SurfaceHolder holder) {
@@ -33,7 +38,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         try {
             mCamera.setPreviewDisplay(holder);
             mCamera.startPreview();
-        } catch (IOException e) {
+        } catch (Exception e) {
             Log.d(TAG, "Error setting camera preview: " + e.getMessage());
         }
     }
