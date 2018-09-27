45c1a6b73ac27251cee371a62edb4eafb79e54fe
Davis Pham <arcentalminitte@gmail.com>
2018-09-26 17:41:48 -0700

    open camera activity

--- app/src/main/java/c7082/davisp/photoapp7082/MainActivity.java
+++ app/src/main/java/c7082/davisp/photoapp7082/MainActivity.java
@@ -1,10 +1,12 @@
 package c7082.davisp.photoapp7082;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +16,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    public void openCameraPreview(View view) {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
 }
