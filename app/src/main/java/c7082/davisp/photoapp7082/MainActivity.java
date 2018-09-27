a610f556a06ca6b6d9e48d284b535f6b6284c54f
Davis Pham <arcentalminitte@gmail.com>
2018-09-26 21:17:45 -0700

    memory database

--- app/src/main/java/c7082/davisp/photoapp7082/MainActivity.java
+++ app/src/main/java/c7082/davisp/photoapp7082/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.hardware.Camera;
+import android.net.ParseException;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -14,8 +15,22 @@ import android.view.View;
 import android.widget.ImageView;
 
 import java.io.IOException;
+import java.util.List;
+
+import c7082.davisp.photoapp7082.data.ImageData;
+import c7082.davisp.photoapp7082.data.ImageDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Database of images
+     */
+    private ImageDatabase database = new ImageDatabase();
+
+    /**
+     * Index of current image displayed
+     */
+    private int imgIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +38,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Opens camera activity
+     * @param view
+     */
     public void openCameraPreview(View view) {
         Intent intent = new Intent(this, CameraActivity.class);
 //        startActivity(intent);
         startActivityForResult(intent, CameraActivity.REQUEST_ID);
+    }
+
+    /**
+     * Goes to next image
+     * @param view
+     */
+    public void nextImage(View view) {
+        List<String> list = database.getListSortedByDate();
+
+        // Skip if no images
+        if (list.size() == 0) {
+            return;
+        }
+
+        imgIndex = (imgIndex + 1) % list.size();
+
+        try {
+            Uri uri = Uri.parse(list.get(imgIndex));
+
+            setImageToDisplay(uri);
+
+        } catch (IOException e) {
+            Log.e("bitmapRegister", e.getMessage());
+        }
+    }
+
+    /**
+     * Goes to prev image
+     * @param view
+     */
+    public void prevImage(View view) {
+        List<String> list = database.getListSortedByDate();
+
+        // Skip if no images
+        if (list.size() == 0) {
+            return;
+        }
+
+        imgIndex = (imgIndex - 1 + list.size()) % list.size();
+
+        try {
+            Uri uri = Uri.parse(list.get(imgIndex));
+
+            setImageToDisplay(uri);
+
+        } catch (IOException e) {
+            Log.e("bitmapRegister", e.getMessage());
+        }
     }
 
     @Override
@@ -41,8 +108,11 @@ public class MainActivity extends AppCompatActivity {
         {
             case CameraActivity.REQUEST_ID:
                 Uri imgUri = data.getData();
+                String imgDate = data.getStringExtra("imgDate");
 
-                RegisterImage(imgUri);
+                RegisterImage(imgUri, imgDate);
+
+                imgIndex++;
 
                 break;
             default:
@@ -53,17 +123,34 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Registers the image to the database
      */
-    private void RegisterImage(Uri imgUri)
-    {
+    private ImageData RegisterImage(Uri imgUri, String imgDate) {
         try {
-            Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imgUri);
+            setImageToDisplay(imgUri);
 
-            ImageView imgView = findViewById(R.id.primaryImage);
+            ImageData imgData = new ImageData(imgUri);
+            imgData.setDateTaken(imgDate);
 
-            imgView.setImageBitmap(bitmap);
+            database.register(imgData);
+
+            return imgData;
 
         } catch (IOException e) {
             Log.e("bitmapRegister", e.getMessage());
         }
+
+        return null;
+    }
+
+    /**
+     * Sets the display image
+     * @param imgUri
+     * @throws IOException
+     */
+    private void setImageToDisplay(Uri imgUri) throws IOException {
+        Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imgUri);
+
+        ImageView imgView = findViewById(R.id.primaryImage);
+
+        imgView.setImageBitmap(bitmap);
     }
 }
